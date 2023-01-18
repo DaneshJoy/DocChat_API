fd4aa3a6aa035abb83f679ed29d5c4714dfb31a1
Saeed Mohagheghi <saeed.discovery@gmail.com>
2023-01-18 21:26:35 +0330

    added haystack app

--- app/docchat_app.py
+++ app/docchat_app.py
@@ -1,8 +1,236 @@
-from fastapi import FastAPI
+import os
+from typing import Optional
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi import File, UploadFile
+from pydantic import BaseModel
+
+from haystack.nodes import Crawler
+from haystack.nodes import PDFToTextConverter
+from haystack.utils import convert_files_to_docs
+from haystack.nodes import PreProcessor
+
+# from haystack.document_stores import MilvusDocumentStore
+from haystack.document_stores import FAISSDocumentStore
+from haystack.nodes import DensePassageRetriever
+from haystack.utils import convert_files_to_docs, clean_wiki_text
+from haystack.nodes import Seq2SeqGenerator
+from haystack.pipelines import GenerativeQAPipeline
+from haystack.pipelines import DocumentSearchPipeline
+from haystack.utils import print_documents, print_answers
 
 app = FastAPI()
 
+DOCS_DIR = 'docs'
+TXT_DIR = 'TXT'
+LINK_DIR = 'links'
+PROCESSED_DOCS = 'processed'
+SQL_FILE = 'faiss_doc_store.db'
+FAISS_FILE = 'faiss_index.faiss'
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+for _dir in [DOCS_DIR, TXT_DIR, LINK_DIR, PROCESSED_DOCS]:
+    if not os.path.exists(_dir):
+        os.makedirs(_dir)
+
+class Url(BaseModel):
+    title: Optional[str] = None
+    url: str
+
+
+@app.get('/')
+async def root(request: Request):
+    html_content = f"""
+    <html>
+        <head>
+            <title>Document Store</title>
+        </head>
+        <body><center>
+        <br/>
+            <h1>֍ Welcome to the Intelligent Document Retrieval API ֍</h1>
+            <h2>► Here is the root URL</h2>
+            <ul style="display:inline;">
+                <li> • Visit <strong>
+                    <a href={request.url._url}docs>{request.url._url}docs/</a>
+                </strong> to view and test API endpoints</li>
+            </ul>
+        </body></center>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.post("/url")
+async def get_url(link: Url):
+    crawler = Crawler(output_dir=LINK_DIR, crawler_depth=1)
+    crawled_docs = crawler.crawl(urls=[link.url])
+    return f"Started processing {link.url}"
+
+
+@app.post("/doc")
+def upload(file: UploadFile = File(...)):
+    try:
+        out_path = os.path.join(DOCS_DIR, file.filename)
+        if os.path.exists(out_path):
+            return {"message": "Uploaded filename already exists in our docs!"}
+        contents = file.file.read()
+        with open(out_path, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+    return {"message": f"Successfully Uploaded {file.filename}"}
+
+
+@app.get("/doc/process")
+def process_docs():    
+    # %% All doc types
+    all_docs = convert_files_to_docs(dir_path=DOCS_DIR)
+
+    for doc_txt in all_docs:
+        doc_filename = f"{os.path.basename(doc_txt.meta['name']).split('.')[0]}.txt"
+        with open(os.path.join(TXT_DIR, doc_filename), 'w') as f:
+            f.write(doc_txt.content)
+
+    # %% Preprocess
+    preprocessor = PreProcessor(
+        clean_empty_lines=True,
+        clean_whitespace=True,
+        clean_header_footer=False,
+        split_by="word",
+        split_length=200,
+        split_overlap=50,
+        split_respect_sentence_boundary=True,
+    )
+
+    docs_default = preprocessor.process(all_docs)
+    print(f"n_docs_input: {len(all_docs)}\nn_docs_output: {len(docs_default)}")
+
+    for chunk in docs_default:
+        chunk_filename = f"{chunk.meta['name']}_{chunk.meta['_split_id']}.txt"
+        with open(os.path.join(PROCESSED_DOCS, chunk_filename), 'w') as f:
+            f.write(chunk.content)
+
+    return {"message": f"Successfully processed {len(all_docs)} document(s) and created {len(docs_default)} passages"}
+
+
+class AiQA:
+    @classmethod
+    def init_doc_store(cls):
+        # cls.document_store = MilvusDocumentStore(host='192.168.1.17',
+        #                             embedding_dim=128,
+        #                             duplicate_documents='overwrite',
+        #                             recreate_index=False)
+
+        cls.document_store = FAISSDocumentStore(embedding_dim=128,
+                                    faiss_index_factory_str="Flat",
+                                    sql_url=f"sqlite:///{SQL_FILE}")
+
+        cls.generator = None
+    
+    
+    # @classmethod
+    # def create_index_milvus(cls, docs):
+    #     cls.document_store.write_documents(docs)
+
+    #     cls.retriever = DensePassageRetriever(
+    #         document_store=cls.document_store,
+    #         query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+    #         passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+    #         use_gpu=True
+    #     )
+
+    #     cls.document_store.update_embeddings(cls.retriever)
+
+
+    @classmethod
+    def create_index_faiss(cls, docs, faiss_path):
+        # cls.document_store= FAISSDocumentStore.load(faiss_path)
+
+        cls.document_store.write_documents(docs)
+
+        # %% Initialize Retriever and Reader/Generator
+
+        # Retriever (DPR)
+
+        cls.retriever = DensePassageRetriever(
+            document_store=cls.document_store,
+            query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+            passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+            use_gpu=False
+        )
+
+        cls.document_store.update_embeddings(cls.retriever)
+    
+    @classmethod
+    def answer(cls, question):
+        if cls.generator is None:
+            cls.generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa",
+                                use_gpu=True)
+            cls.pipe = GenerativeQAPipeline(cls.generator, cls.retriever)
+        k_retriever = 3
+        cls.res = cls.pipe.run(question, params={"Retriever": {"top_k": k_retriever}})
+
+        print_answers(cls.res, details="minimum")
+
+
+@app.get("/ai/index")
+def index_documents():
+    # # Convert files to docs + cleaning
+    docs = convert_files_to_docs(dir_path=PROCESSED_DOCS,
+                                clean_func=clean_wiki_text,
+                                split_paragraphs=True)
+    
+    AiQA.init_doc_store()
+    # AiQA.create_index_milvus(docs)
+    AiQA.create_index_faiss(docs, "faiss_index.faiss")
+
+    # %% ----------------MILVUS----------------
+    # document_store = MilvusDocumentStore(host='192.168.1.17',
+    #                                 embedding_dim=128,
+    #                                 duplicate_documents='overwrite',
+    #                                 recreate_index=False)
+
+    # document_store.write_documents(docs)
+
+    # retriever = DensePassageRetriever(
+    #     document_store=document_store,
+    #     query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+    #     passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+    #     use_gpu=True
+    # )
+
+    # document_store.update_embeddings(retriever)
+
+    
+
+    # Test DPR
+    p_retrieval = DocumentSearchPipeline(AiQA.retriever)
+    res = p_retrieval.run(query="Tell me something about global greenhouse gas emissions?", params={"Retriever": {"top_k": 5}})
+    print_documents(res, max_text_len=512)
+
+    return {"message": f"Successfully indexed processed passages"}
+
+@app.get('/ai/answer/{question}')
+def answer(question: str):
+    # %% Generator
+    # generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa",
+    #                             use_gpu=True)
+
+    # pipe = GenerativeQAPipeline(generator, AiQA.retriever)
+
+    # k_retriever = 3
+    # res = pipe.run(question, params={"Retriever": {"top_k": k_retriever}})
+
+    # print_answers(res, details="minimum")
+    AiQA.answer(question)
+    for i in range(3):
+        print( AiQA.res['answers'][0].meta['content'][i])
+    return {'answer': AiQA.res['answers'][0].answer}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
+    # uvicorn.run(app, port=8080, host='localhost')
+    
--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/saeed/anaconda3/etc/profile.d/conda.sh
-conda activate deploy
-# uvicorn docchat_app:app --host 0.0.0.0 --port 8088 --reload
-gunicorn -k uvicorn.workers.UvicornWorker docchat_app:app
+# source /home/saeed/anaconda3/etc/profile.d/conda.sh
+# conda activate deploy
+# uvicorn docchat_app:app --host 0.0.0.0 --port 80 --reload
+gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 docchat_app:app --workers 5
