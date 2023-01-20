154530fce069d974669258838661e43f10386b42
Saeed Mohagheghi <saeed.discovery@gmail.com>
2023-01-20 11:30:41 +0330

    changed AiQA class

--- app/docchat_app.py
+++ app/docchat_app.py
@@ -36,9 +36,75 @@ for _dir in [DOCS_DIR, TXT_DIR, LINK_DIR, PROCESSED_DOCS]:
     if not os.path.exists(_dir):
         os.makedirs(_dir)
 
+
 class Url(BaseModel):
     title: Optional[str] = None
     url: str
+
+
+class AiQA:
+    def __init__(self):
+        # cls.document_store = MilvusDocumentStore(host='192.168.1.17',
+        #                             embedding_dim=128,
+        #                             duplicate_documents='overwrite',
+        #                             recreate_index=False)
+
+        if os.path.exists(SQL_FILE) and os.path.exists('faiss_doc_store.json'):
+            self.document_store = FAISSDocumentStore.load(SQL_FILE)
+        else:
+            self.document_store = FAISSDocumentStore(embedding_dim=128,
+                                        faiss_index_factory_str="Flat",
+                                        sql_url=f"sqlite:///{SQL_FILE}")
+
+        self.retriever = None
+        self.generator = None
+
+
+    # @classmethod
+    # def create_index_milvus(cls, docs):
+    #     cls.document_store.write_documents(docs)
+
+    #     cls.retriever = DensePassageRetriever(
+    #         document_store=cls.document_store,
+    #         query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+    #         passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+    #         use_gpu=True
+    #     )
+
+    #     cls.document_store.update_embeddings(cls.retriever)
+
+
+    def create_index_faiss(self, docs, faiss_path):
+        # cls.document_store= FAISSDocumentStore.load(faiss_path)
+
+        self.document_store.write_documents(docs)
+
+        # %% Initialize Retriever and Reader/Generator
+
+        # Retriever (DPR)
+        if self.retriever == None:
+            self.retriever = DensePassageRetriever(
+                document_store=self.document_store,
+                query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+                passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+                use_gpu=False
+            )
+
+        self.document_store.update_embeddings(retriever=self.retriever,
+            update_existing_embeddings=False)
+
+    def answer(self, question):
+        if self.generator is None:
+            self.generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa",
+                                use_gpu=True)
+            self.pipe = GenerativeQAPipeline(self.generator, self.retriever)
+        k_retriever = 3
+        self.res = self.pipe.run(question, params={"Retriever": {"top_k": k_retriever}})
+
+        print_answers(self.res, details="minimum")
+
+
+aiqa = AiQA()
 
 
 @app.get('/')
@@ -119,80 +185,17 @@ def process_docs():
 
     return {"message": f"Successfully processed {len(all_docs)} document(s) and created {len(docs_default)} passages"}
 
+
 @app.post('/doc/get_docs')
 async def get_docs(files: List[UploadFile]):
     for file in files:
         contents = await file.read()
         with open(os.path.join(PROCESSED_DOCS, file.filename), 'wb') as f:
             f.write(contents)
+
     index_documents()
     # return {"filenames": [file.filename for file in files]}
     return {f"Received and indexed {len(files)} documents"}
-
-
-class AiQA:
-    @classmethod
-    def init_doc_store(cls):
-        # cls.document_store = MilvusDocumentStore(host='192.168.1.17',
-        #                             embedding_dim=128,
-        #                             duplicate_documents='overwrite',
-        #                             recreate_index=False)
-
-        if os.path.exists(SQL_FILE) and os.path.exists('faiss_doc_store.json'):
-            cls.document_store = FAISSDocumentStore.load(SQL_FILE)
-        else:
-            cls.document_store = FAISSDocumentStore(embedding_dim=128,
-                                        faiss_index_factory_str="Flat",
-                                        sql_url=f"sqlite:///{SQL_FILE}")
-
-        cls.retriever = None
-        cls.generator = None
-
-
-    # @classmethod
-    # def create_index_milvus(cls, docs):
-    #     cls.document_store.write_documents(docs)
-
-    #     cls.retriever = DensePassageRetriever(
-    #         document_store=cls.document_store,
-    #         query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
-    #         passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
-    #         use_gpu=True
-    #     )
-
-    #     cls.document_store.update_embeddings(cls.retriever)
-
-
-    @classmethod
-    def create_index_faiss(cls, docs, faiss_path):
-        # cls.document_store= FAISSDocumentStore.load(faiss_path)
-
-        cls.document_store.write_documents(docs)
-
-        # %% Initialize Retriever and Reader/Generator
-
-        # Retriever (DPR)
-        if cls.retriever == None:
-            cls.retriever = DensePassageRetriever(
-                document_store=cls.document_store,
-                query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
-                passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
-                use_gpu=False
-            )
-
-        cls.document_store.update_embeddings(retriever=cls.retriever,
-            update_existing_embeddings=False)
-    
-    @classmethod
-    def answer(cls, question):
-        if cls.generator is None:
-            cls.generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa",
-                                use_gpu=True)
-            cls.pipe = GenerativeQAPipeline(cls.generator, cls.retriever)
-        k_retriever = 3
-        cls.res = cls.pipe.run(question, params={"Retriever": {"top_k": k_retriever}})
-
-        print_answers(cls.res, details="minimum")
 
 
 @app.get("/ai/index")
@@ -201,10 +204,9 @@ def index_documents():
     docs = convert_files_to_docs(dir_path=PROCESSED_DOCS,
                                 clean_func=clean_wiki_text,
                                 split_paragraphs=True)
-    
-    AiQA.init_doc_store()
+
     # AiQA.create_index_milvus(docs)
-    AiQA.create_index_faiss(docs, "faiss_index.faiss")
+    aiqa.create_index_faiss(docs, "faiss_index.faiss")
 
     # %% ----------------MILVUS----------------
     # document_store = MilvusDocumentStore(host='192.168.1.17',
@@ -225,10 +227,10 @@ def index_documents():
 
     
 
-    # Test DPR
-    p_retrieval = DocumentSearchPipeline(AiQA.retriever)
-    res = p_retrieval.run(query="Tell me something about global greenhouse gas emissions?", params={"Retriever": {"top_k": 5}})
-    print_documents(res, max_text_len=512)
+    # # Test DPR
+    # p_retrieval = DocumentSearchPipeline(AiQA.retriever)
+    # res = p_retrieval.run(query="Tell me something about global greenhouse gas emissions?", params={"Retriever": {"top_k": 5}})
+    # print_documents(res, max_text_len=512)
 
     if os.path.exists(PROCESSED_DOCS):
         shutil.rmtree(PROCESSED_DOCS)
@@ -249,10 +251,10 @@ def answer(question: str):
     # res = pipe.run(question, params={"Retriever": {"top_k": k_retriever}})
 
     # print_answers(res, details="minimum")
-    AiQA.answer(question)
+    aiqa.answer(question)
     for i in range(3):
-        print( AiQA.res['answers'][0].meta['content'][i])
-    return {'answer': AiQA.res['answers'][0].answer}
+        print(aiqa.res['answers'][0].meta['content'][i])
+    return {'answer': aiqa.res['answers'][0].answer}
 
 if __name__ == '__main__':
     import uvicorn
