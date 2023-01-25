cfac1f49c113bc015ca1d8e332a3cb51200f94a2
Saeed Mohagheghi <saeed.discovery@gmail.com>
2023-01-26 01:16:55 +0330

    changed run.sh port to 8088

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # source /home/saeed/anaconda3/etc/profile.d/conda.sh
 # conda activate deploy
-uvicorn docchat_app:app --host 0.0.0.0 --port 8080 --reload
+uvicorn docchat_app:app --host 0.0.0.0 --port 8088 --reload
 # gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 docchat_app:app --workers 5
